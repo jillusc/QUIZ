cdf8d32595415ece8ef1a600ec762e61e2ad497f
jillusc <>
2023-10-13 14:44:31 +0000

    Added main run_game function, implemented ANSI colours

--- questions.py
+++ questions.py
@@ -1,204 +1,211 @@
-question_pack_1 = "Pack 1"
-question_pack_2 = "Pack 2"
-question_pack_3 = "Pack 3"
-
 question_packs = {
-    question_pack_1: [
-        {
-            "question": "Which language gave us the name Santa Claus?",
-            "options": ["A. Dutch", "B. Spanish", "C. Hungarian"],
-            "correct_answer": "A",
-        },
-        {
-            "question": '''Who added "galumph" and "chortle" to the
-                            English language?''',
-            "options": [
-                "A. Charles Dickens",
-                "B. Lewis Carroll",
-                "C. William Shakespeare",
-            ],
-            "correct_answer": "B",
-        },
-        {
-            "question": '''Which Bronte novel\'s final chapter begins,
-                           "Reader, I married him"?''',
-            "options": [
-                "A. Pride and Prejudice",
-                "B. Wuthering Heights",
-                "C. Jane Eyre",
-            ],
-            "correct_answer": "C",
-        },
-        {
-            "question": "Cerulean is a shade of which colour?",
-            "options": ["A. blue", "B. pink", "C. green"],
-            "correct_answer": "A",
-        },
-        {
-            "question": "Which company produced the first mobile phone?",
-            "options": ["A. Sony Ericsson", "B. Motorola", "C. Nokia"],
-            "correct_answer": "B",
-        },
-        {
-            "question": "Greenland is classed as part of which continent?",
-            "options": ["A. Europe", "B. Asia", "C. North America"],
-            "correct_answer": "C",
-        },
-        {
-            "question": """In what type of buildings do Americans spend
-                            the most time other than home and workplace?""",
-            "options": ["A. shopping malls", "B. churches", "C. hospitals"],
-            "correct_answer": "A",
-        },
-        {
-            "question": "What is the clavicle commonly known as?",
-            "options": ["A. the knee cap", "B. the collar bone", """C. the jaw
-                          bone"""],
-            "correct_answer": "B",
-        },
-        {
-            "question": "What is the lens of the eye called?",
-            "options": ["A. the retina", "B. the macula", "C. the cornea"],
-            "correct_answer": "C",
-        },
-        {
-            "question": """What is the name of the cat owned by the family of a
-                            dog called Odie?""",
-            "options": ["A. Garfield", "B. Sylvester", "C. Felix"],
-            "correct_answer": "A",
-        },
-    ],
-    question_pack_2: [
-        {
-            "question": """What was jazz star John Coltrane's favoured
-                            instrument?""",
-            "options": ["A. trumpet", "B. saxophone", "C. piano"],
-            "correct_answer": "B",
-        },
-        {
-            "question": """Which Irvine Welsh novel became one of 1996's
-                            top films?""",
-            "options": [
-                "A. Fear and Loathing in Las Vegas",
-                "B. The Silence of the Lambs",
-                "C. Trainspotting",
-            ],
-            "correct_answer": "C",
-        },
-        {
-            "question": """Whose ship was the first to circumnavigate
-                            the globe?""",
-            "options": [
-                "A. Ferdinand Magellan",
-                "B. James Cook",
-                "C. Christopher Columbus",
-            ],
-            "correct_answer": "A",
-        },
-        {
-            "question": """Who was the first confirmed climber to reach the
-                            summit of Mt. Everest?""",
-            "options": ["A. Ranulph Fiennes", "B. Edmund Hillary",
-                        "C. Captain Scott"],
-            "correct_answer": "B",
-        },
-        {
-            "question": "Who discovered penicillin?",
-            "options": ["A. Edward Jenner", "B. Joseph Lister", """C.
-                          Alexander Fleming"""],
-            "correct_answer": "C",
-        },
-        {
-            "question": "Who invented the television?",
-            "options": [
-                "A. John Logie-Baird",
-                "B. Thomas Edison",
-                "C. Alexander Graham Bell",
-            ],
-            "correct_answer": "A",
-        },
-        {
-            "question": "What type of animal is a caiman?",
-            "options": ["A. insect", "B. reptile", "C. mammal"],
-            "correct_answer": "B",
-        },
-        {
-            "question": "What is measured using the Beaufort scale?",
-            "options": [
-                "A. earthquake magnitude",
-                "B. atmospheric pressure",
-                "C. wind speed",
-            ],
-            "correct_answer": "C",
-        },
-        {
-            "question": """What's the most widely distributed bird in
-                            the world?""",
-            "options": ["A. the sparrow", "B. the goose", "C. the pigeon"],
-            "correct_answer": "A",
-        },
-        {
-            "question": "From what does paella get its name?",
-            "options": ["A. pie", "B. pan", "C. pot"],
-            "correct_answer": "B",
-        },
-    ],
-    question_pack_3: [
-        {
-            "question": "What is the world's most consumed cheese?",
-            "options": ["A. camembert", "B. cheddar", "C. mozzarella"],
-            "correct_answer": "C",
-        },
-        {
-            "question": "Which metal has the chemical symbol Au?",
-            "options": ["A. gold", "B. uranium", "C. silver"],
-            "correct_answer": "A",
-        },
-        {
-            "question": """From which country does the sparkling wine
-                            cava originate?""",
-            "options": ["A. Italy", "B. Spain", "C. Greece"],
-            "correct_answer": "B",
-        },
-        {
-            "question": """Which measurement of energy takes its name from
-                            a Latin word for heat?""",
-            "options": ["A. joule", "B. volt", "C. calorie"],
-            "correct_answer": "C",
-        },
-        {
-            "question": """Which illness is so-called because it was supposedly
-                            caused by the evil influence of the stars?""",
-            "options": ["A. the flu", "B. cancer", "C. rickets"],
-            "correct_answer": "A",
-        },
-        {
-            "question": """Which spice is widely known in Indian cooking as
-                           'the queen of spices'?""",
-            "options": ["A. clove", "B. cardamom", "C. cumin"],
-            "correct_answer": "B",
-        },
-        {
-            "question": "What do we call a female donkey?",
-            "options": ["A. dorrie", "B. ellen", "C. jenny"],
-            "correct_answer": "C",
-        },
-        {
-            "question": "Which mammal has the most powerful bite?",
-            "options": ["A. hippo", "B. possum", "C. wolf"],
-            "correct_answer": "A",
-        },
-        {
-            "question": """Which common food takes its name from an
-                            Anglo-Saxon word meaning 'mouldy'?""",
-            "options": ["A. yoghurt", "B. cheese", "C. bread"],
-            "correct_answer": "B",
-        },
-        {
-            "question": """If a dish is 'a la florentine', what ingredient is
-                            it assumed to have?""",
-            "options": ["A. eggs", "B. ham", "C. spinach"],
-            "correct_answer": "C",
-        },
-    ],
+    1: {
+        "name": "Pack 1",
+        "questions": [
+            {
+                "question": "Which language gave us the name Santa Claus?",
+                "options": ["A. Dutch", "B. Spanish", "C. Hungarian"],
+                "correct_answer": "A",
+            },
+            {
+                "question": '''Who added "galumph" and "chortle" to the
+                                English language?''',
+                "options": [
+                    "A. Charles Dickens",
+                    "B. Lewis Carroll",
+                    "C. William Shakespeare",
+                ],
+                "correct_answer": "B",
+            },
+            {
+                "question": '''Which Bronte novel\'s final chapter begins,
+                               "Reader, I married him"?''',
+                "options": [
+                    "A. Pride and Prejudice",
+                    "B. Wuthering Heights",
+                    "C. Jane Eyre",
+                ],
+                "correct_answer": "C",
+            },
+            {
+                "question": "Cerulean is a shade of which colour?",
+                "options": ["A. blue", "B. pink", "C. green"],
+                "correct_answer": "A",
+            },
+            {
+                "question": "Which company produced the first mobile phone?",
+                "options": ["A. Sony Ericsson", "B. Motorola", "C. Nokia"],
+                "correct_answer": "B",
+            },
+            {
+                "question": "Greenland is classed as part of which continent?",
+                "options": ["A. Europe", "B. Asia", "C. North America"],
+                "correct_answer": "C",
+            },
+            {
+                "question": """In what type of buildings do Americans spend
+                               the most time other than home and workplace?""",
+                "options": ["A. shopping malls", "B. churches",
+                            """C. hospitals"""],
+                "correct_answer": "A",
+            },
+            {
+                "question": "What is the clavicle commonly known as?",
+                "options": ["A. the knee cap", "B. the collar bone", """C. the
+                            jaw bone"""],
+                "correct_answer": "B",
+            },
+            {
+                "question": "What is the lens of the eye called?",
+                "options": ["A. the retina", "B. the macula", "C. the cornea"],
+                "correct_answer": "C",
+            },
+            {
+                "question": """What is the name of the cat owned by the family
+                               of a dog called Odie?""",
+                "options": ["A. Garfield", "B. Sylvester", "C. Felix"],
+                "correct_answer": "A",
+            },
+        ]
+    },
+    2: {
+        "name": "Pack 2",
+        "questions": [
+            {
+                "question": """What was jazz star John Coltrane's favoured
+                                instrument?""",
+                "options": ["A. trumpet", "B. saxophone", "C. piano"],
+                "correct_answer": "B",
+            },
+            {
+                "question": """Which Irvine Welsh novel became one of 1996's
+                                top films?""",
+                "options": [
+                    "A. Fear and Loathing in Las Vegas",
+                    "B. The Silence of the Lambs",
+                    "C. Trainspotting",
+                ],
+                "correct_answer": "C",
+            },
+            {
+                "question": """Whose ship was the first to circumnavigate
+                                the globe?""",
+                "options": [
+                    "A. Ferdinand Magellan",
+                    "B. James Cook",
+                    "C. Christopher Columbus",
+                ],
+                "correct_answer": "A",
+            },
+            {
+                "question": """Who was the first confirmed climber to reach the
+                                summit of Mt. Everest?""",
+                "options": ["A. Ranulph Fiennes", "B. Edmund Hillary",
+                            "C. Captain Scott"],
+                "correct_answer": "B",
+            },
+            {
+                "question": "Who discovered penicillin?",
+                "options": ["A. Edward Jenner", "B. Joseph Lister", """C.
+                              Alexander Fleming"""],
+                "correct_answer": "C",
+            },
+            {
+                "question": "Who invented the television?",
+                "options": [
+                    "A. John Logie-Baird",
+                    "B. Thomas Edison",
+                    "C. Alexander Graham Bell",
+                ],
+                "correct_answer": "A",
+            },
+            {
+                "question": "What type of animal is a caiman?",
+                "options": ["A. insect", "B. reptile", "C. mammal"],
+                "correct_answer": "B",
+            },
+            {
+                "question": "What is measured using the Beaufort scale?",
+                "options": [
+                    "A. earthquake magnitude",
+                    "B. atmospheric pressure",
+                    "C. wind speed",
+                ],
+                "correct_answer": "C",
+            },
+            {
+                "question": """What's the most widely distributed bird in
+                                the world?""",
+                "options": ["A. the sparrow", "B. the goose", "C. the pigeon"],
+                "correct_answer": "A",
+            },
+            {
+                "question": "From what does paella get its name?",
+                "options": ["A. pie", "B. pan", "C. pot"],
+                "correct_answer": "B",
+            },
+        ]
+    },
+    3: {
+        "name": "Pack 3",
+        "questions": [
+            {
+                "question": "What is the world's most consumed cheese?",
+                "options": ["A. camembert", "B. cheddar", "C. mozzarella"],
+                "correct_answer": "C",
+            },
+            {
+                "question": "Which metal has the chemical symbol Au?",
+                "options": ["A. gold", "B. uranium", "C. silver"],
+                "correct_answer": "A",
+            },
+            {
+                "question": """From which country does the sparkling wine
+                                cava originate?""",
+                "options": ["A. Italy", "B. Spain", "C. Greece"],
+                "correct_answer": "B",
+            },
+            {
+                "question": """Which measurement of energy takes its name from
+                                a Latin word for heat?""",
+                "options": ["A. joule", "B. volt", "C. calorie"],
+                "correct_answer": "C",
+            },
+            {
+                "question": """Which illness is so-called because it was
+                                supposedly caused by the evil influence
+                                of the stars?""",
+                "options": ["A. the flu", "B. cancer", "C. rickets"],
+                "correct_answer": "A",
+            },
+            {
+                "question": """Which spice is widely known in Indian cooking as
+                               'the queen of spices'?""",
+                "options": ["A. clove", "B. cardamom", "C. cumin"],
+                "correct_answer": "B",
+            },
+            {
+                "question": "What do we call a female donkey?",
+                "options": ["A. dorrie", "B. ellen", "C. jenny"],
+                "correct_answer": "C",
+            },
+            {
+                "question": "Which mammal has the most powerful bite?",
+                "options": ["A. hippo", "B. possum", "C. wolf"],
+                "correct_answer": "A",
+            },
+            {
+                "question": """Which common food takes its name from an
+                                Anglo-Saxon word meaning 'mouldy'?""",
+                "options": ["A. yoghurt", "B. cheese", "C. bread"],
+                "correct_answer": "B",
+            },
+            {
+                "question": """If a dish is 'a la florentine', what ingredient
+                                is it assumed to contain?""",
+                "options": ["A. eggs", "B. ham", "C. spinach"],
+                "correct_answer": "C",
+            },
+        ]
+    }
 }
--- run.py
+++ run.py
@@ -6,6 +6,7 @@ import random
 
 # ANSI escape codes for colours
 YELLOW = "\033[33m"
+GREEN = "\033[32m"
 RESET = "\033[0m"
 
 SCOPE = [
@@ -30,7 +31,7 @@ def welcome_screen():
     via the txt file, the game is explained;
     then the function requests username input
     and validates the data entered.
-    The input string and the final message both 
+    The input string and the final message both
     begin with spaces in order to position the
     text with an apt indent.
     """
@@ -40,23 +41,24 @@ def welcome_screen():
 
     while True:
         try:
-            name = input("        To start, please enter a username: ")
+            name = input("         To start, please enter a username: ")
             if not name.strip():
                 raise ValueError("A username is required.")
             if not (3 <= len(name) <= 10 and name.isalnum()):
-                raise ValueError("Username must be 3-10 alphanumeric characters.")
+                raise ValueError("""Username must be 3-10 alphanumeric
+            characters.""")
         except ValueError as e:
-            print(f"Invalid. {e}\n")
+            print(f"\033[33mInvalid.\033[0m {e}\n")
         else:
             print()
-            print(f"        Let's go then, {name}.")
-            print()
+            print(f"         Let's go then, {name}.")
             return name
+
 
 name = welcome_screen()
 
 
-def choose_question_pack(name):
+def choose_question_pack():
     """
     This function loads the visual of question packs
     and requests the user to select one of the three
@@ -66,35 +68,70 @@ def choose_question_pack(name):
     with open('question_pack_visual.txt', 'r') as file:
         content = file.read()
         print(content)
-        
+
         while True:
             try:
                 questions = input("Choose a question pack: ")
                 if questions not in ['1', '2', '3']:
                     raise ValueError("Please enter 1, 2, or 3.")
             except ValueError as e:
-                print(f"Invalid. {e}\n")
+                print(f"\033[33mInvalid.\033[0m {e}\n")
             else:
                 print()
                 print(f"Alright! Question pack {questions} it is.\n")
                 return questions
 
 
-choose_question_pack(name)
+questions = int(choose_question_pack())
 
 
-def display_positive_response(name):
+def run_game(question_pack, name):
     """
-    The computer generates a random response
-    to a correct answer given by the user.
+    The main game function. It shuffles and then displays
+    the questions and the answer options that are stored
+    in questions.py. Then it prompts the user's answer
+    input, validates it for A, B or C only, evaluates
+    the answer, provides an appropriate response and
+    increments the score.
     """
+    score = 0
+    question_number = 1
     positive_responses = [
         "Well done.",
         f"Nice one, {name}.",
         f"Good call, {name}.",
         "Great.",
-        "That's right.",
+        "Alright.",
         "Very good."
-        ]
+    ]
 
-    return random.choice(positive_response)
+    random.shuffle(question_pack["questions"])
+
+    for question_data in question_pack["questions"]:
+        question = question_data["question"]
+        options = question_data["options"]
+        correct_answer = question_data["correct_answer"]
+
+        print()
+        print(f"Q{question_number}. {question}\n")
+
+        for option in options:
+            print(option)
+        print()
+        while True:
+            user_answer = input("What's your answer: A, B or C? ").upper()
+            if user_answer in ["A", "B", "C"]:
+                break
+            else:
+                print("Invalid answer. Please enter A, B or C.")
+
+        if user_answer.upper() == correct_answer:
+            response = random.choice(positive_responses)
+            print(f"\033[32mCorrect! {response}\033[0m\n")
+            score += 1
+        else:
+            print(f"\033[33mSorry! That's incorrect.\033[0m\n")
+        question_number += 1
+
+
+run_game(question_packs[questions], name)
